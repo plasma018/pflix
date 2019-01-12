d525cad69a12f08d5b256979b89773fb28b9f2bb
Piova294 <zero2904@gmail.com>
2019-01-12 17:07:57 +0800

    add title card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { CarouselComponent } from './carousel/carousel.component';
+import { TitleCardComponent } from './carousel/title-card/title-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    CarouselComponent
+    CarouselComponent,
+    TitleCardComponent
   ],
   imports: [
     BrowserModule
